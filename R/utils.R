d269baa55e8d1c55b1c699833f3a9885fcbf1e64
Kenny Shirley <kennetheshirley@yahoo.com>
2014-05-05 17:05:45 -0400

    updated utils.R with normalize() instead of norm()

--- R/utils.R
+++ R/utils.R
@@ -27,19 +27,7 @@ su <- function(x, ...) {
   sort(unique(x, ...))
 }
 
-#' @title Sum
-#'
-#' @description This function is an alias for sum(x, na.rm=TRUE).
-#'
-#' @param x a vector or a data frame or an array or NULL.
-#' @export
-#' @examples
-#' x <- c(1, 1, 2, 2, 3)
-#' sum.na(x)
-
-sum.na <- function(x) sum(x, na.rm=TRUE)
-
-#' @title Norm
+#' @title Normalize
 #'
 #' @description This function is an alias for x/sum(x).
 #'
@@ -47,18 +35,27 @@ sum.na <- function(x) sum(x, na.rm=TRUE)
 #' @export
 #' @examples
 #' x <- c(1, 1, 2, 2, 3)
-#' norm(x)
+#' normalize(x)
 
-norm <- function(x) x/sum(x)
+normalize <- function(x) x/sum(x)
 
 #' @title Entropy
 #'
-#' @description This function calculates entropy
+#' @description This function calculates the entropy of a discrete (categorical) probability distribution
 #'
-#' @param x a vector or a data frame or an array or NULL.
+#' @param x a vector that contains either a normalized or un-normalized vector of probabilities of a 
+#' discrete (categorical) distribution
 #' @export
 #' @examples
 #' x <- c(1, 1, 2, 2, 3)
 #' ent(x)
+#' y <- c(1/9, 1/9, 2/9, 2/9, 3/9)
+#' ent(y) # should be same as ent(x)
+#' z <- c(1/3, 1/3, 1/6, 1/6)
+#' ent(z)
+#' z2 <- c(1/3, 1/3, 1/6, 1/6, 0)
+#' ent(z2) # should be the same as ent(z), since this version of entropy assumes 0*log(0) = 0
 
-ent <- function(x) -sum(x[x!=0]/sum(x[x!=0])*log(x[x!=0]/sum(x[x!=0])))
+ent <- function(x) -sum(x[x != 0]/sum(x[x != 0])*log(x[x != 0]/sum(x[x != 0])))
+
+

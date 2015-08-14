14ab130ab581e44e33f0c777e88ccd5da8d87722
kshirley <kennetheshirley@yahoo.com>
2015-08-14 13:13:39 -0400

    fixed typo

--- src/lda.c
+++ src/lda.c
@@ -244,7 +244,7 @@ void sample(int first, int last, double *prob, int *z){
   int j, l = last - first + 1;
   double x, s;
   //x = drand48();
-  x = (double() rand() / (RAND_MAX));
+  x = ((double) rand() / (RAND_MAX));
   s = 0.0;
   for (j = 0; j < l; j++){
     s += prob[j];
